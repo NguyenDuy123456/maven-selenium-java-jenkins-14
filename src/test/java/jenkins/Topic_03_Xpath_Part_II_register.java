6d2d5ed88f2c099dcb27d6064cb5dca58f0b3a85
Nguyen Duy <nguyenthanhloan.259@gmail.com>
2022-07-26 03:42:47 +0200

    add

--- src/test/java/jenkins/Topic_03_Xpath_Part_II_register.java
+++ src/test/java/jenkins/Topic_03_Xpath_Part_II_register.java
@@ -26,7 +26,7 @@ public class Topic_03_Xpath_Part_II_register {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		//driver.get("");
+		
 	}
 
 	@Test
